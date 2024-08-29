a85a4d879feb3811e9c2e006b75cb5a2397518b3
kevintsai <kevintsai@TD-ID10-01460.taidoc.com.tw>
2024-08-29 17:23:09 +0800

    first release

---
+++ src/main/java/com/kt/ai/SpringBootAiDay25Application.java
@@ -0,0 +1,14 @@
+package com.kt.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAiDay25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAiDay25Application.class, args);
+	}
+
+	
+}
